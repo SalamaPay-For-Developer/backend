9c70c14f7601ae7ea0ed2be619344b4d229e73d4
raydanielg <airezra2+1@gmail.com>
2026-08-09 00:29:21 +0300

    commit

--- apps/wallets/urls.py
+++ apps/wallets/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from .views import WalletViewSet, LedgerEntryViewSet
 
 router = DefaultRouter()
+router.register(r'wallets', WalletViewSet)
+router.register(r'ledger', LedgerEntryViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- apps/wallets/views.py
+++ apps/wallets/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from django.db import models
+from .models import Wallet, LedgerEntry
+from .serializers import WalletSerializer, LedgerEntrySerializer
+from apps.accounts.models import User
 
-# Create your views here.
+
+class WalletViewSet(viewsets.ModelViewSet):
+    queryset = Wallet.objects.all()
+    serializer_class = WalletSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == User.Role.ADMIN:
+            return Wallet.objects.all()
+        return Wallet.objects.filter(
+            models.Q(owner=user) | models.Q(business__owner=user) | models.Q(business__members__user=user)
+        ).distinct()
+
+
+class LedgerEntryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = LedgerEntry.objects.all()
+    serializer_class = LedgerEntrySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == User.Role.ADMIN:
+            return LedgerEntry.objects.all()
+        return LedgerEntry.objects.filter(
+            models.Q(wallet__owner=user) | models.Q(wallet__business__owner=user) | models.Q(wallet__business__members__user=user)
+        ).distinct()
